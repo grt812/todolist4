05912601bfc2e9c55265f682117d5c01dc18c56d
Garrett Gee <garrett.m.gee@gmail.com>
2021-01-11 17:27:07 -0700

    todolist4 created from newtabnotes

--- content.js
+++
@@ -1,8 +0,0 @@
-console.log("loaded script");
-window.addEventListener("message", function(event) {
-    if (event.source != window)
-        return;
-    if (event.data.type && (event.data.type == "FROM_PAGE")) {
-        console.log("bruh");
-    }
-});
